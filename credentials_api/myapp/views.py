f2e71b83a8ee427de5ce77349c77a7d121bb985b
vasitha1 <sulemfelsi@gmail.com>
2025-01-30 20:33:03 +0000

    Correcting git url and iso format date and time

--- credentials_api/myapp/views.py
+++ credentials_api/myapp/views.py
@@ -3,9 +3,13 @@ from django.http import JsonResponse
 from datetime import datetime
 
 def my_credentials(request):
+
+    current_datetime_utc = datetime.now(timezone.utc)
+    current_datetime_iso = current_datetime_utc.isoformat().replace('+00:00', 'Z')
+
     data = {
         'email': 'sulemfelsi@gmail.com',
-        'current_datetime': datetime.now().isoformat(),
-        'github_url': 'https://github.com/vasitha1/hng0_user_credentials_api.git'
+        'current_datetime': current_datetime_iso,
+        'github_url': 'https://github.com/vasitha1/hng0_user_credentials_api'
     }
     return JsonResponse(data)
